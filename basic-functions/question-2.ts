a92c136ef60a48bd5dbea711e6aa2fc765ea34b8
gabrielcouto <goldevil2015@gmail.com>
2025-04-28 04:44:07 -0300

    feat: add basics question 2

---
+++ basic-functions/question-2.ts
@@ -0,0 +1,12 @@
+//GETS INT AND RETURNS TRUE IF EVEN AND FALSE IF ODD
+
+function isPar(num: number): any{
+    if (num % 2 === 0) {
+        return "true"
+    }
+    else {
+        return "false"
+    }
+}
+
+console.log(isPar(11))
